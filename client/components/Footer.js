f8131030c87479df73751d782a221ed6c1c2f6e1
Zaina Rodney <zainarodney@gmail.com>
2021-04-04 00:28:56 -0400

    updated dependencies

--- client/components/Footer.js
+++ client/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer>
-      <small>&#169; 2021, Zaina Rodney</small><br/>
+      <small>&#169; 2021 Zaina Rodney</small><br/>
       <small>Photo by <a href="https://unsplash.com/@jcdilorenzo?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Jackie DiLorenzo</a> on <a href="https://unsplash.com/s/photos/plants?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
     </small>
     </footer>
